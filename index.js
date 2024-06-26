a412ff28110d6f9ea96cb15b93d403960c24d3e9
Hemanth-05 <hemanthnath123@gmail.com>
2024-06-26 11:07:33 +0530

    Second

--- index.js
+++ index.js
@@ -5,3 +5,20 @@ $(".project").on("mouseenter", function(){
 $(".project").on("mouseleave", function(){
     $(this).find(".projectImage").removeClass("moveLeft");
 });
+
+document.querySelectorAll('.navItem').forEach(link => {
+    link.addEventListener('click', function(event) {
+      event.preventDefault();
+      const targetId = this.getAttribute('href').substring(1);
+      const targetElement = document.getElementById(targetId);
+      const offset = 10; // Adjust this value as needed (10vh offset)
+      const bodyRect = document.body.getBoundingClientRect().top;
+      const elementRect = targetElement.getBoundingClientRect().top;
+      const offsetPosition = elementRect - bodyRect - (window.innerHeight * offset / 100);
+
+      window.scrollTo({
+        top: offsetPosition,
+        behavior: 'smooth'
+      });
+    });
+  });
